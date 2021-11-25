7b140781f3df64cf744645eed2b2da6059fdd4a0
maggie slein <maggieslein77@gmail.com>
2021-11-25 00:01:27 -0800

    half of scholander extracted, fiddling with the units conversions (definitely not right but getting closer possibly?)

--- lichen_Screening.R
+++ lichen_Screening.R
@@ -11,7 +11,7 @@ screen_abstracts(lichen2)
 ##ask mary about the difference in units between the spreadsheet 
 ## and the plos one figure 1....
 lichen_data <- read_csv("extraction/lichen_data.csv") %>%
-  mutate(temp_dep=1/((8.617333262145*10^-5)*(temp+273.15)),
+  mutate(inv_T=1/((8.617333262145*10^-5)*(temp+273.15)),
          mmol_co2 = case_when(gas_unit %in% c("mg") ~ (1/44.01), 
                                   gas_unit %in% c("nmol") ~ (0.000001),
                                   gas_unit %in% c("umol") ~ (0.0001)),
@@ -21,7 +21,7 @@ lichen_data <- read_csv("extraction/lichen_data.csv") %>%
          per_min = case_when(time %in% c("hr") ~ (1/60),
                              time %in% c("sec") ~ (60), 
                              TRUE ~ (1)),
-         std_response = response_value*(mmol_co2*mg_sample*per_min))
+         mmol_mg_min = response_value*(mmol_co2*mg_sample^-1*per_min^-1))
        
 
 subset_lichen<- lichen_data %>%
@@ -34,10 +34,13 @@ subset_lichen<- lichen_data %>%
 TRUE ~ "photosynthesis"))
   
                                 
-ggplot(subset_lichen, aes(x=temp_dep, y=std_response, color=broad_responses))+
+ggplot(subset_lichen, aes(x=desc(inv_T), y=log(mmol_mg_min), color=broad_responses))+
   geom_smooth(method="lm")+
   stat_poly_eq(formula = my.formula, 
                aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
                parse = TRUE)+
-  geom_point()
-  #facet_wrap(~broad_responses)
+  geom_point(alpha=0.5)
+  #facet_wrap(~avg_ppfd)
+ggplot(subset_lichen, aes(x=avg_ppfd, fill=broad_responses))+
+  geom_histogram(binwidth=20)
+  
